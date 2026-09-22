38778cda84791e3c60c5b18bad63c7e51066da07
Michael Kuhn <mkuhn@nlr.gov>
2026-09-22 09:50:15 -0600

    add another resolution to HOS-NWT plot

--- Kynema_SGF_cases/irregular_tank/IrregularNWT.jl
+++ Kynema_SGF_cases/irregular_tank/IrregularNWT.jl
@@ -22,6 +22,7 @@ else
 end
 
 file_amrwind = "data/amr_wind_hos_nwt_sampling_time_series.txt"
+file_amrwind2 = "data/amr_wind_hos_nwt_sampling_time_series_finer.txt"
 file_hosnwt = "data/HOS_NWT_probes.dat"
 
 rows_to_skip = 49
@@ -45,6 +46,12 @@ amrwind_data = Float64.(amrwind_data)
 amrwind_time = amrwind_data[:,1]
 amrwind_wave = amrwind_data[:,4]
 
+amrwind_data2 = readdlm(file_amrwind2)
+amrwind_data2 = amrwind_data2[2:end,1:4]
+amrwind_data2 = Float64.(amrwind_data2)
+amrwind_time2 = amrwind_data2[:,1]
+amrwind_wave2 = amrwind_data2[:,4]
+
 # Experiment
 file_exp = "/Users/mkuhn/Library/CloudStorage/OneDrive-NLR/testruns_data/OC6_PhaseIa_dataset_and_more/OC6_LC33_LC53_CFD_scripts_data/LC33_FixedStructure/ZXP0_LC33_full.txt"
 ZXP0_data = readdlm(file_exp)
@@ -56,9 +63,10 @@ EXP_wave = ZXP0_data[:,2]
 
 # Plot Waves Time series (Model Scale)
 rescaling = 50.
-Time_series_plot = plot(EXP_time./sqrt(rescaling),EXP_wave./(rescaling),label="Experiment",color=:black)
+Time_series_plot = plot(EXP_time./sqrt(rescaling),EXP_wave./(rescaling),label="Experiment",color=:black,legend=:topright)
 plot!(HOS_NWT_Case4_time,HOS_NWT_Case4_wave,label="HOS-NWT", color=:blue)
-plot!(amrwind_time,amrwind_wave,label="Kynema-SGF", color=:red, linestyle=:dot)
+plot!(amrwind_time,amrwind_wave,label="Kynema-SGF, \$\\Delta{z}\$ = 0.0188m", color=:green, linestyle=:dashdot)
+plot!(amrwind_time2,amrwind_wave2,label="Kynema-SGF, \$\\Delta{z}\$ = 0.0094m", color=:red, linestyle=:dot)
 xlabel!("Time [sec]")
 ylabel!("Wave Elevation [m]")
 savefig(Time_series_plot,"plotting_outputs/wave_elevation_time_series.pdf")
@@ -66,11 +74,12 @@ savefig(Time_series_plot,"plotting_outputs/wave_elevation_time_series.pdf")
 ### **** PSD COMPUTATIONS AND PLOTS ******
 stepRange = collect(3000:108000)
 f, P1, PSD, fdouble, P2 = FFTAnalysis(EXP_time[stepRange]./sqrt(rescaling),EXP_wave[stepRange]./rescaling,true,false,smoothing_type,smoothing_parameter)
-wave_plot = plot(f[1:end],abs.(PSD[1:end]),yscale=:log10,ylims=(1e-7,1e-2),label="Experiment",xlims=(0,1.5),color=:black,linewidth=2)
+wave_plot = plot(f[1:end],abs.(PSD[1:end]),yscale=:log10,ylims=(1e-7,1e-2),label="Experiment",xlims=(0,1.5),color=:black,linewidth=2,legend=:bottomright)
 
 f, P1, PSD, fdouble, P2 = FFTAnalysis(HOS_NWT_Case4_time,HOS_NWT_Case4_wave,true,false,smoothing_type,smoothing_parameter)
 plot!(f[1:end],abs.(PSD[1:end]),yscale=:log10,label="HOS-NWT",color=:blue,linewidth=2)
 
+# Coarser AMR-Wind simulation
 dt_array = zeros(length(amrwind_data[:,1]))
 for ii=1:length(dt_array)-1
     local dt = amrwind_data[ii+1,1].-amrwind_data[ii,1]
@@ -86,7 +95,25 @@ AMRWind_time = amr_time_evenly_spaced
 AMRWind_Wave = amrwind_wave_evenly_spaced
 
 f, P1, PSD, fdouble, P2 = FFTAnalysis(AMRWind_time,AMRWind_Wave,true,false,smoothing_type,smoothing_parameter)
-plot!(f[1:end],abs.(PSD[1:end]),yscale=:log10,label="Kynema-SGF",color=:red,linestyle=:dot,linewidth=2)
+plot!(f[1:end],abs.(PSD[1:end]),yscale=:log10,label="Kynema-SGF, \$\\Delta{z}\$ = 0.0188m",color=:green,linestyle=:dashdot,linewidth=2)
+
+# Finer AMR-Wind simulation
+dt_array = zeros(length(amrwind_data2[:,1]))
+for ii=1:length(dt_array)-1
+    local dt = amrwind_data2[ii+1,1].-amrwind_data2[ii,1]
+    dt_array[ii] = dt
+end
+t_spacing = median(dt_array)
+
+AMRWind_WAVE_time_interp = Interpolations.deduplicate_knots!(amrwind_time2)
+itp_waves_amrwind =  interpolate((AMRWind_WAVE_time_interp,), amrwind_wave2, Gridded(Linear()))
+amr_time_evenly_spaced = collect(amrwind_data2[1,1]:t_spacing:amrwind_data2[end,1])
+amrwind_wave_evenly_spaced = itp_waves_amrwind.(amr_time_evenly_spaced)
+AMRWind_time = amr_time_evenly_spaced
+AMRWind_Wave = amrwind_wave_evenly_spaced
+
+f, P1, PSD, fdouble, P2 = FFTAnalysis(AMRWind_time,AMRWind_Wave,true,false,smoothing_type,smoothing_parameter)
+plot!(f[1:end],abs.(PSD[1:end]),yscale=:log10,label="Kynema-SGF, \$\\Delta{z}\$ = 0.0094m",color=:red,linestyle=:dot,linewidth=2)
 
 xlabel!("Frequency [Hz]")
 ylabel!("PSD of Incident Wave [m\$^2\$/Hz]")
